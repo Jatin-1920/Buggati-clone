4a7d619379f5ea7d8c1d9589b4b46032ed5914c4
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-03-30 10:56:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const open = document.querySelector('#open')
 const close = document.querySelector('#close')
 const nav = document.querySelector('.nav')
+const link = nav.querySelectorAll(".link")
 open.addEventListener("click",function(){
     open.style.transform = "rotate(1turn)"
 nav.style.transform="translateY(0)"
@@ -12,4 +13,12 @@ close.addEventListener("click",function() {
     nav.style.transform="translateY(-100%)"
     close.style.display = "none"
 open.style.display="block"
+})
+
+link.forEach(li=>{
+    li.addEventListener("click",function() {
+    nav.style.transform="translateY(-100%)"
+    close.style.display = "none"
+open.style.display="block"
+})
 })
