0e5e4831137c7fca0edad141062b5974dff04f87
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-03-30 10:04:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const nav = document.querySelector('.primary-nav')
 const primary = document.querySelector('.primary-navigation')
 open.addEventListener("click",function(){
     open.style.transform = "rotate(1turn)"
-nav.style.transform = "scaleY(1)"
+nav.style.transform="translateY(0)"
 open.style.display="none"
 close.style.display= "block"
-primary.classList.add("delaya")
 })
 
 close.addEventListener("click",function() {
-    nav.style.transform="scaleY(0)"
+    nav.style.transform="translateY(-100%)"
     close.style.display = "none"
 open.style.display="block"
 })
